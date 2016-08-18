c3023041bc5e7eb983cbb8fca3e098cfc217eae3
vitaly <goodgame1945@gmail.com>
2016-08-18 10:06:25 +0300

    fixing table

--- gkn/models.py
+++ gkn/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Order(models.Model):
-    id = models.IntegerField(max_length=5, primary_key=True)
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=30)
     total = models.FloatField()
     email = models.CharField(max_length=30)
